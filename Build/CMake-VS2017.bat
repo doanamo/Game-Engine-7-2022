31741aa11d5b5b4e1734f13bb757d62d7c0db589
Piotr Doan <doanpiotr@gmail.com>
2018-06-04 20:41:51 +0200

    Fix batch file launching the solution after cmake command fails

--- Build/CMake-VS2017.bat
+++ Build/CMake-VS2017.bat
@@ -11,6 +11,11 @@ cd "CMake-VS2017"
 rem Generate solution files.
 cmake -G "Visual Studio 15" ./../..
 
+if %ERRORLEVEL% NEQ 0 (
+    pause
+    exit
+)
+
 rem Open the solution file.
 echo -- Opening the solution file..
 start "" "GameEngine.sln"
