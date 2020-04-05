edf5ab3731a605af0a9ec41ec9b609d11746972c
Piotr Doan <doanpiotr@gmail.com>
2020-04-05 05:03:27 +0200

    Create game engine project with tests

--- Source/Empty.cpp
+++ Source/Empty.cpp
@@ -1,5 +1,3 @@
 /*
     Copyright (c) 2018-2020 Piotr Doan. All rights reserved.
 */
-
-#include "Precompiled.hpp"
--- Tests/Helpers/TestHelpers.cpp
+++ Tests/Helpers/TestHelpers.cpp
@@ -1,10 +1,3 @@
 /*
     Copyright (c) 2018-2020 Piotr Doan. All rights reserved.
 */
-
-#include "TestHelpers.hpp"
-
-int main()
-{
-    return 0;
-}
