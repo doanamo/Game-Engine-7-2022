b273269751455a29ea90a348a5b7de16cb813703
Piotr Doan <doanpiotr@gmail.com>
2020-04-05 05:08:53 +0200

    Fix CMake utility batch files

--- Build/CMake-Example-VS2019-x86.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-rem Copyright (c) 2018-2020 Piotr Doan. All rights reserved.
-call "../Example/Build/CMake-VS2019-x64.bat" "../Example/Build" "CMake-Example-VS2019-x86" Win32
--- Example/Build/CMake-VS2019-x86.bat
+++ Example/Build/CMake-VS2019-x86.bat
@@ -1,4 +1,4 @@
 @echo off
 
 rem Copyright (c) 2018-2020 Piotr Doan. All rights reserved.
-call "CMake-VS2019-x64.bat" "." "CMake-VS2019-x86" Win32
+call "CMake-VS2019-x64.bat" "../." "CMake-VS2019-x86" Win32
