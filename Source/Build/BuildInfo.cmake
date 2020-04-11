69b2f703bd8f1e456406a0f3f31e0f72b35d9298
Piotr Doan <doanpiotr@gmail.com>
2020-04-11 21:56:59 +0200

    Change way revisions are counted for build info

--- Source/Build/BuildInfo.cmake
+++ Source/Build/BuildInfo.cmake
@@ -1,5 +1,5 @@
 # Execute Git commands to retrieve information from the version control.
-execute_process(COMMAND "${GIT_EXECUTABLE}" -C "${ENGINE_PROJECT_DIR}" rev-list --count --first-parent HEAD
+execute_process(COMMAND "${GIT_EXECUTABLE}" -C "${ENGINE_PROJECT_DIR}" rev-list --all --count
     OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE GIT_ENGINE_CHANGE_NUMBER)
 
 execute_process(COMMAND "${GIT_EXECUTABLE}" -C "${ENGINE_PROJECT_DIR}" log -1 --format=%h
@@ -11,7 +11,7 @@ execute_process(COMMAND "${GIT_EXECUTABLE}" -C "${ENGINE_PROJECT_DIR}" log -1 --
 execute_process(COMMAND "${GIT_EXECUTABLE}" -C "${ENGINE_PROJECT_DIR}" rev-parse --abbrev-ref HEAD
     OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE GIT_ENGINE_BRANCH_NAME)
 
-execute_process(COMMAND "${GIT_EXECUTABLE}" -C "${GAME_PROJECT_DIR}" rev-list --count --first-parent HEAD
+execute_process(COMMAND "${GIT_EXECUTABLE}" -C "${GAME_PROJECT_DIR}" rev-list --all --count
     OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE GIT_GAME_CHANGE_NUMBER)
 
 execute_process(COMMAND "${GIT_EXECUTABLE}" -C "${GAME_PROJECT_DIR}" log -1 --format=%h
