b560efbbe1e6bba7a174b0bc1f740d61889bfce9
Piotr Doan <doanpiotr@gmail.com>
2020-05-21 15:26:12 +0200

    Remove redundant texture load from SpriteDemo

--- Example/Source/Scenes/SpriteDemo.cpp
+++ Example/Source/Scenes/SpriteDemo.cpp
@@ -90,38 +90,26 @@ SpriteDemo::CreateResult SpriteDemo::Create(Engine::Root* engine)
         // Create transform component.
         auto* transform = instance->m_gameState->componentSystem->Create<Game::TransformComponent>(cameraEntity);
         ASSERT(transform != nullptr, "Could not create a transform component!");
-
         transform->SetPosition(glm::vec3(0.0f, 0.0f, 2.0f));
 
         // Create camera component.
         auto* camera = instance->m_gameState->componentSystem->Create<Game::CameraComponent>(cameraEntity);
         ASSERT(camera != nullptr, "Could not create a camera component!");
-
         camera->SetupOrthogonal(glm::vec2(16.0f, 9.0f), 0.1f, 1000.0f);
     }
 
     // Create player entity.
     {
-        // Load texture.
-        Graphics::Texture::LoadFromFile textureParams;
-        textureParams.services = &engine->GetServices();
-        textureParams.filePath = "Data/Textures/Checker.png";
-
-        Graphics::TexturePtr texture = resourceManager->Acquire<Graphics::Texture>(
-            textureParams.filePath, textureParams).UnwrapEither();
-
         // Create named entity.
         Game::EntityHandle playerEntity = instance->m_gameState->entitySystem->CreateEntity();
         instance->m_gameState->identitySystem->SetEntityName(playerEntity, "Player");
 
         // Create transform component.
         auto* transform = instance->m_gameState->componentSystem->Create<Game::TransformComponent>(playerEntity);
-
         transform->SetPosition(glm::vec3(0.0f, 0.0f, 0.0f));
 
         // Create sprite component.
         auto* sprite = instance->m_gameState->componentSystem->Create<Game::SpriteComponent>(playerEntity);
-
         sprite->SetRectangle(glm::vec4(-0.5f, -0.5f, 0.5f, 0.5f));
         sprite->SetColor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
         sprite->SetTextureView(textureAtlas->GetRegion("animation_frame_3"));
@@ -130,7 +118,6 @@ SpriteDemo::CreateResult SpriteDemo::Create(Engine::Root* engine)
 
         // Create sprite animation component.
         auto* spriteAnimation = instance->m_gameState->componentSystem->Create<Game::SpriteAnimationComponent>(playerEntity);
-
         spriteAnimation->SetSpriteAnimationList(spriteAnimationList);
         spriteAnimation->Play("rotation", true);
     }
