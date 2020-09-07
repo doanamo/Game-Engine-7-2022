35e2bec55e344081c5a7be89a5b91118726b5b9f
Piotr Doan <doanpiotr@gmail.com>
2020-09-07 10:33:25 +0200

    Add empty unit test that can be used as an example template

---
+++ Tests/Empty/TestEmpty.cpp
@@ -0,0 +1,15 @@
+/*
+    Copyright (c) 2018-2020 Piotr Doan. All rights reserved.
+*/
+
+#include <TestHelpers.hpp>
+
+int TestEmpty()
+{
+    return true;
+}
+
+int main()
+{
+    TEST_RUN(TestEmpty);
+}
