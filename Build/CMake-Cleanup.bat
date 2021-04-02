097f19b685f22fdd2cf002c140b777c481684e1d
Piotr Doan <doanpiotr@gmail.com>
2021-04-02 18:07:29 +0200

    Minor build batch cleanup

--- Build/CMake-Cleanup.bat
+++ Build/CMake-Cleanup.bat
@@ -2,18 +2,16 @@
 
 rem Copyright (c) 2018-2021 Piotr Doan. All rights reserved.
 rem Software distributed under the permissive MIT License.
-echo -- Cleaning CMake directories...
 
-rem Remove temporary directories.
+echo -- Cleaning CMake directories...
 :delete_folders
 for /d %%x in ("CMake-*") do rd /s /q "%%x" >nul 2>&1
 
-rem Retry if some directories cannot be deleted.
 for /d %%x in ("CMake-*") do (
     echo Could not delete some files. Making another attempt in 4 seconds...
     timeout /t 4 >nul 2>&1
     goto delete_folders
 )
 
-rem Remove temporary files.
+echo -- Cleaning temporary files...
 for %%x in ("*.tmp") do rm /s /q "%%x" >nul 2>&1
--- Build/CMake-VS2019-x64.bat
+++ Build/CMake-VS2019-x64.bat
@@ -2,9 +2,6 @@
 
 rem Copyright (c) 2018-2021 Piotr Doan. All rights reserved.
 rem Software distributed under the permissive MIT License.
-echo -- Generating CMake project...
-
-rem Collect command line arguments.
 set inputDir=%1
 set outputDir=%2
 set arch=%3
@@ -13,11 +10,10 @@ if [%inputDir%] == [] set inputDir="."
 if [%outputDir%] == [] set outputDir="CMake-VS2019-x64"
 if [%arch%] == [] set arch=x64
 
-rem Create empty project directory.
 mkdir %outputDir% >nul 2>&1
 cd %outputDir%
 
-rem Generate solution files.
+echo -- Generating CMake project...
 set archArg=-A %arch%
 
 cmake -G "Visual Studio 16" %archArg% "../%inputDir%"
@@ -27,12 +23,11 @@ if %ERRORLEVEL% NEQ 0 (
     exit
 )
 
-rem Open main solution file.
+echo -- Opening project solution...
 for /f %%f in ('dir /b "*.sln"') do (
     set solution=%%f
     goto found
 )
 :found
 
-echo -- Opening solution file...
 start "" "%solution%"
