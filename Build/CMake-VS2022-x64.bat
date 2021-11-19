22909606118b08421ff055f513fa35f03d6a7bec
Piotr Doan <doanpiotr@gmail.com>
2021-11-19 18:06:31 +0100

    Add support for generating Visual Studio 2022 solution

---
+++ Build/CMake-VS2022-x64.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal
+
+rem Copyright (c) 2018-2021 Piotr Doan. All rights reserved.
+rem Software distributed under the permissive MIT License.
+
+set inputDir=%1
+set outputDir=%2
+set arch=%3
+
+if [%inputDir%] == [] set inputDir="../."
+if [%outputDir%] == [] set outputDir="CMake-VS2022-x64"
+if [%arch%] == [] set arch=x64
+
+echo ---- Preparing build directory...
+mkdir %outputDir% >nul 2>&1
+cd %outputDir%
+
+echo ---- Generating CMake project...
+set archArg=-A %arch%
+
+cmake -G "Visual Studio 17" %archArg% "../%inputDir%"
+
+if %ERRORLEVEL% NEQ 0 (
+    pause
+    exit
+)
+
+echo ---- Opening project solution...
+for /f %%f in ('dir /b "*.sln"') do (
+    set solution=%%f
+    goto found
+)
+:found
+
+start "" "%solution%"
---
+++ Build/CMake-VS2022-x86.bat
@@ -0,0 +1,4 @@
+@echo off
+setlocal
+
+call "CMake-VS2022-x64.bat" "../." "CMake-VS2022-x86" Win32
