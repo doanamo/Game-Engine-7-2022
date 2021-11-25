65d72649724a494ab7ed549066d2f15ec3c77137
Piotr Doan <doanpiotr@gmail.com>
2021-11-25 18:06:08 +0100

    Various fixes for Emscripten build script

--- Build/CMake-Emscripten-Release.bat
+++ Build/CMake-Emscripten-Release.bat
@@ -15,9 +15,17 @@ if [%outputDir%] == [] set outputDir="CMake-Emscripten-Release"
 if [%configType%] == [] set configType="Release"
 if [%projectPage%] == [] set projectPage="Example/Example/Example.html"
 
+echo ---- Checking Python availability....
+python --version 2>NUL
+if %ERRORLEVEL% NEQ 0 (
+    echo Python could not be found!
+    pause
+    exit /b
+)
+
 echo ---- Preparing Emscripten SDK...
 if not exist "CMake-Emscripten-SDK" (
-    git clone --depth=1 --branch=master https://github.com/emscripten-core/emsdk.git "CMake-Emscripten-SDK"
+    git clone -c advice.detachedHead=false --depth=1 --branch=3.0.0 https://github.com/emscripten-core/emsdk.git "CMake-Emscripten-SDK"
     
     cd "CMake-Emscripten-SDK"
     call emsdk.bat install latest
@@ -34,11 +42,11 @@ mkdir %outputDir% >nul 2>&1
 cd %outputDir%
 
 echo ---- Preparing Ninja build system...
-if not exist "ninja" (
-    git clone --depth=1 --branch=release https://github.com/ninja-build/ninja.git
+if not exist "Ninja" (
+    git clone -c advice.detachedHead=false --depth=1 --branch=v1.10.2 https://github.com/ninja-build/ninja.git "Ninja"
     
-    mkdir "ninja/build" >nul 2>&1
-    cd "ninja/build"
+    mkdir "Ninja/build" >nul 2>&1
+    cd "Ninja/build"
     cmake "../." -DBUILD_TESTING=0
     cmake --build . --config Release >nul 2>&1
     cd "../../."
@@ -52,7 +60,7 @@ cmake --build . --config Release >nul 2>&1
 cd "../."
 
 echo ---- Generating CMake project...
-cmake -G "Ninja" "../%inputDir%" -DCMAKE_TOOLCHAIN_FILE="../CMake-Emscripten-SDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" -DCMAKE_MAKE_PROGRAM="%cd%/ninja/build/release/ninja.exe" -DCMAKE_BUILD_TYPE=%configType%
+cmake -G "Ninja" "../%inputDir%" -DCMAKE_TOOLCHAIN_FILE="../CMake-Emscripten-SDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" -DCMAKE_MAKE_PROGRAM="%cd%/Ninja/build/release/ninja.exe" -DCMAKE_BUILD_TYPE=%configType%
 
 if %ERRORLEVEL% NEQ 0 (
     pause
